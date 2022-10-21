93fa85e78133a5b2dbf97cb5bf3f4499ce2f8ad8
rizon <rizonrahat199@gmail.com>
2022-10-21 15:46:43 +0600

    2nd

---
+++ src/AuthContext/AuthProvider.js
@@ -0,0 +1,14 @@
+import React, { createContext } from 'react';
+import UseFirebase from '../Hooks/useFirebase';
+export const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+    const allContexts = UseFirebase();
+    return (
+        <AuthContext.Provider value={allContexts}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA34vARaPMx0qmPu0fHvOyfI9ATAYHJpPA",
+  authDomain: "vromon-bangladesh.firebaseapp.com",
+  projectId: "vromon-bangladesh",
+  storageBucket: "vromon-bangladesh.appspot.com",
+  messagingSenderId: "657233760694",
+  appId: "1:657233760694:web:5d0ed157c1d7760d56b117"
+   
+};
+
+export default firebaseConfig;
